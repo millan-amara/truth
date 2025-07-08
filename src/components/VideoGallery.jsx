51f4b9a29a78843b1783cf1c9527ea13363be069
Hughes <onyangohughes8611@gmail.com>
2025-07-08 22:00:14 +0300

    added video

--- src/components/VideoGallery.jsx
+++ src/components/VideoGallery.jsx
@@ -12,26 +12,33 @@ const videos = [
     orientation: "portrait",
     poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639832/truth/gallery-image/yellow_zcpuof.png"
   },
-    {
+  {
     src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750637791/truth/2025-04-16-Nature_zhtjow.mp4",
     orientation: "landscape",
     poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639832/truth/gallery-image/nature_p5hwcb.png"
   },
-    {
+  {
     src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750637742/truth/00033_sasvoq.mp4",
     orientation: "portrait",
     poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639830/truth/gallery-image/coca_yrzaze.png"
+  },
+
+  {
+    src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750636442/truth/255_ykvzsu.mp4",
+    orientation: "portrait",
+    poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639830/truth/gallery-image/bikey_aegcdt.png"
+  },
+  {
+    src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1752000489/truth/Gratitude_kgdtaf.mp4",
+    orientation: "landscape",
+    poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1752000649/truth/gallery-image/one_odtjxp.png"
   },
   {
     src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750637774/truth/212_i27pao.mp4",
     orientation: "landscape",
     poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639831/truth/gallery-image/leaf_b1tl7g.png"
   },
-      {
-    src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750636442/truth/255_ykvzsu.mp4",
-    orientation: "portrait",
-    poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639830/truth/gallery-image/bikey_aegcdt.png"
-  },
+
   {
     src: "https://res.cloudinary.com/dcj3qavxy/video/upload/v1750637749/truth/45105_bk8qoa.mp4",
     poster: "https://res.cloudinary.com/dcj3qavxy/image/upload/v1750639831/truth/gallery-image/red_dw8yw4.png",
